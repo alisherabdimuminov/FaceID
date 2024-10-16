62ad0f4b792f56f2b88d758ebc2548caf03495d9
alishercpp <alisher.abdimuminov.2005@gmail.com>
2024-10-16 14:38:59 +0500

    adding CSRF_TRUSTED_ORIGINS config

--- config/settings.py
+++ config/settings.py
@@ -104,3 +104,5 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": ["rest_framework.authentication.TokenAuthentication", ],
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
 }
+
+CSRF_TRUSTED_ORIGINS = ["https://fc.uzfi.uz", "https://www.fc.uzfi.uz"]
