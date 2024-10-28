6567b6b597cbf126c0c6ed21aaf94ec5a734addf
alishercpp <alisher.abdimuminov.2005@gmail.com>
2024-10-28 14:45:24 +0500

    bug fix

--- config/settings.py
+++ config/settings.py
@@ -93,6 +93,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
+CSRF_TRUSTED_ORIGINS = ["https://api.fc.uzfi.uz"]
+SESSION_COOKIE_DOMAIN = [".uzfi.uz"]
+CSRF_COOKIE_DOMAIN = [".uzfi.uz"]
 
 AUTH_USER_MODEL = "users.User"
 
