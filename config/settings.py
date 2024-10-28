edeccdfd6afc2e5f503525a3fd27091e9cf3b918
alishercpp <alisher.abdimuminov.2005@gmail.com>
2024-10-28 10:53:44 +0500

    bug fix

--- config/settings.py
+++ config/settings.py
@@ -96,11 +96,10 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "https://api.fc.uzfi.uz",
+    "https://api.fc.uzfi.uz/",
 ]
 CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:3000",
-    "https://api.fc.uzfi.uz",
+    "https://api.fc.uzfi.uz/",
 ]
 
 AUTH_USER_MODEL = "users.User"
