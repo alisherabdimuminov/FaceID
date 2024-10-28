9c43c3a9a89468fd9fd9010c4f2aa44b3ecd98af
alishercpp <alisher.abdimuminov.2005@gmail.com>
2024-10-28 11:02:49 +0500

    bug fix

--- config/settings.py
+++ config/settings.py
@@ -94,12 +94,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "https://api.fc.uzfi.uz/",
-]
 CSRF_TRUSTED_ORIGINS = [
     "https://api.fc.uzfi.uz/",
+    "https://api.fc.uzfi.uz",
 ]
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
